fe8f0b8d017cc608f9b4a30c402347f50b46f224
Luca Donadello <45766869+LucaDonadello@users.noreply.github.com>
2024-10-12 15:54:12 -0500

    add docker file frontend

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
-      '/ideas': 'http://localhost:3001',
-      '/auth': 'http://localhost:3001'
+      '/ideas': 'http://host.docker.internal:3001',
+      '/auth': 'http://host.docker.internal:3001'
     }
   },
   plugins: [react()],
